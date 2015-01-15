2a7c803d7c5703ed6d01b252a508aa9ee4529876
Rishi Gupta12 <gupt21@gmail.com>
2015-01-15 11:51:22 +0530

    delete to restructure native library project files

--- com.embeddedunveiled.serial.native/Release/src/windows_serial_lib.d
+++
@@ -1,7 +0,0 @@
-src/windows_serial_lib.d: ../src/windows_serial_lib.c \
- /home/r/packages/jdk/jdk1.6.0_45/include/jni.h \
- /home/r/packages/jdk/jdk1.6.0_45/include/linux/jni_md.h
-
-/home/r/packages/jdk/jdk1.6.0_45/include/jni.h:
-
-/home/r/packages/jdk/jdk1.6.0_45/include/linux/jni_md.h:
