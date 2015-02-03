08ec4bcb0e7e6f34cacff0abd6c76c1c66a5d58e
Rishi Gupta12 <gupt21@gmail.com>
2015-02-03 15:45:52 +0530

    add project directory for mac os

---
+++ com.embeddedunveiled.native/ReadMe.txt
@@ -0,0 +1,12 @@
+========================================================================
+
+Windows : windows_serial folder contains native shared library developed in visual studio.
+
+Linux : linux_serial directory contains eclipse CDT project using gcc toolchain.
+
+Mac OS x : macOS_serial directory contains eclipse CDT project using gcc/xcode/os x toolchain.
+
+The linux and mac share common header file interface to java layer.
+
+========================================================================
+
---
+++ com.embeddedunveiled.native/macOS_serial/Debug/src/unix_like_serial.d
@@ -0,0 +1,13 @@
+src/unix_like_serial.d: ../src/unix_like_serial.c \
+  /System/Library/Frameworks/JavaVM.framework/Versions/A/Headers/jni.h \
+  /System/Library/Frameworks/JavaVM.framework/Versions/A/Headers/jni_md.h \
+  ../src/unix_like_serial_lib.h \
+  ../src/../../com_embeddedunveiled_serial_SerialComJNINativeInterface.h
+
+/System/Library/Frameworks/JavaVM.framework/Versions/A/Headers/jni.h:
+
+/System/Library/Frameworks/JavaVM.framework/Versions/A/Headers/jni_md.h:
+
+../src/unix_like_serial_lib.h:
+
+../src/../../com_embeddedunveiled_serial_SerialComJNINativeInterface.h:
