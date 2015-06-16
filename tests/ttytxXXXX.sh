cefa0ed730f577944f1bffa6b8241c8417842e66
root <root@r-office.(none)>
2015-06-16 15:27:33 +0530

    add scripts to create dev node for ttytxXXXX in linux

---
+++ tests/ttytxXXXX.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Run as root user
+
+mknod /dev/ttytx0003 c 63 2
+mknod /dev/ttytx0004 c 63 3
+mknod /dev/ttytx0005 c 63 5
+mknod /dev/ttytx0006 c 63 6
+mknod /dev/ttytx0007 c 63 7
+mknod /dev/ttytx0089 c 63 4
+mknod /dev/ttytx0763 c 63 9
