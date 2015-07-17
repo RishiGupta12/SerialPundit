d390801f4026461682700adfceadc5d840aee12e
Rishi Gupta12 <gupt21@gmail.com>
2015-07-17 23:27:42 +0530

    expand dynamic array at correct sieze value in C

--- com.embeddedunveiled.native/linux_serial/src/unix_like_util.c
+++ com.embeddedunveiled.native/linux_serial/src/unix_like_util.c
@@ -20,23 +20,37 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <errno.h>
 #include "unix_like_serial_lib.h"
 
+/* Allocate memory of given size and initializes elements as appropriate. */
 void init_array_list(struct array_list *al, int initial_size) {
-  al->base = (char **) calloc(initial_size, sizeof(char *));
-  al->index = 0;
-  al->current_size = initial_size;
+	al->base = (char **) calloc(initial_size, sizeof(char *));
+	if(al->base == NULL) {
+		fprintf(stderr, "array calloc %s %d\n", "failed : ", errno);
+		fflush(stderr);
+	}
+	al->index = 0;
+	al->current_size = initial_size;
 }
 
+/* Insert given string at next position expanding memory size allocated
+ * if required. */
 void insert_array_list(struct array_list *al, char *element) {
-  if(al->index > al->current_size) {
-    al->current_size = al->current_size * 2;
-    al->base = (char **) realloc(al->base, al->current_size * sizeof(char *));
-  }
-  al->base[al->index] = element;
-  al->index++;
+	if(al->index >= al->current_size) {
+		al->current_size = al->current_size * 2;
+		al->base = (char **) realloc(al->base, al->current_size * sizeof(char *));
+		if(al->base == NULL) {
+			fprintf(stderr, "array realloc %s %d\n", "failed : ", errno);
+			fflush(stderr);
+		}
+	}
+	al->base[al->index] = element;
+	al->index++;
 }
 
+/* Releases memory allocated to each elements and release the memory
+ * allocated to the list itself as well. */
 void free_array_list(struct array_list *al) {
 	int x = 0;
 	for (x=0; x < al->index; x++) {
