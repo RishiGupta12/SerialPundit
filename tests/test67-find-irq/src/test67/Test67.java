52f906b5d2b7384b9434bfbaa8a6a8aa8a396136
Rishi Gupta12 <gupt21@gmail.com>
2015-08-20 12:59:22 +0530

    add test for finding IRQ and serial port address

---
+++ tests/test67-find-irq/src/test67/Test67.java
@@ -0,0 +1,59 @@
+/**
+ * Author : Rishi Gupta
+ * 
+ * This file is part of 'serial communication manager' library.
+ *
+ * The 'serial communication manager' is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * The 'serial communication manager' is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with serial communication manager. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package test67;
+
+import com.embeddedunveiled.serial.SerialComManager;
+
+public class Test67  {
+
+	public static SerialComManager scm = null;
+	public static String PORT = null;
+	public static String PORT1 = null;
+
+	public static void main(String[] args) {
+		try {
+			scm = new SerialComManager();
+
+			int osType = scm.getOSType();
+			if(osType == SerialComManager.OS_LINUX) {
+				PORT = "/dev/ttyUSB0";
+				PORT1 = "/dev/ttyS1";
+			}else if(osType == SerialComManager.OS_WINDOWS) {
+				PORT = "COM51";
+				PORT1 = "COM52";
+			}else if(osType == SerialComManager.OS_MAC_OS_X) {
+				PORT = "/dev/cu.usbserial-A70362A3";
+				PORT1 = "/dev/cu.usbserial-A602RDCH";
+			}else if(osType == SerialComManager.OS_SOLARIS) {
+				PORT = null;
+				PORT1 = null;
+			}else{
+			}
+			
+			long handle = scm.openComPort(PORT, true, true, true);
+			System.out.println(PORT + " :: " + scm.findIRQnumberForComPort(handle));
+			scm.closeComPort(handle);
+			
+			long handle1 = scm.openComPort(PORT1, true, true, true);
+			System.out.println(PORT1 + " :: " + scm.findIRQnumberForComPort(handle1));
+			scm.closeComPort(handle1);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
