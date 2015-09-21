7f1b9e389653b4af803481358faf52098bad9af9
Rishi Gupta12 <gupt21@gmail.com>
2015-09-21 14:01:47 +0530

    port FTDI D2XX library in mac os x

---
+++ vendor-libs/ftdi/d2xx/d2xx-mac/src/scm_ftdi_d2xx.h
@@ -0,0 +1,168 @@
+/***************************************************************************************************
+ * Author : Rishi Gupta
+ *
+ * This file is part of 'serial communication manager' library.
+ *
+ * The 'serial communication manager' is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * The 'serial communication manager' is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with serial communication manager. If not, see <http://www.gnu.org/licenses/>.
+ *
+ ***************************************************************************************************/
+
+#ifndef SCM_FTDI_D2XX_LIB_H_
+#define SCM_FTDI_D2XX_LIB_H_
+
+#include <jni.h>
+#include "WinTypes.h"    /* FTDI D2XX header file from vendor   */
+#include "ftd2xx.h"      /* FTDI D2XX header file from vendor   */
+
+/* these constants (SCM_) values must match their values in java layer */
+
+#define SCM_DEVICE_UNKNOWN  0X01
+#define SCM_DEVICE_AM       0x02
+#define SCM_DEVICE_BM       0x03
+#define SCM_DEVICE_100AX    0X04
+#define SCM_DEVICE_232B     0X05
+#define SCM_DEVICE_232R     0X06
+#define SCM_DEVICE_232H     0X07
+#define SCM_DEVICE_2232C    0X08
+#define SCM_DEVICE_2232H    0X09
+#define SCM_DEVICE_4232H    0X10
+#define SCM_DEVICE_X_SERIES 0X11
+
+#define SCM_OPEN_BY_SERIAL_NUMBER 0x08
+#define SCM_OPEN_BY_DESCRIPTION   0x10
+#define SCM_OPEN_BY_LOCATION      0x20
+
+#define SCM_GENERIC_READ  0x01
+#define SCM_GENERIC_WRITE 0x02
+
+#define SCM_SETRTS      1
+#define SCM_CLRRTS      2
+#define SCM_SETDTR      3
+#define SCM_CLRDTR      4
+#define SCM_SETBREAK    5
+#define SCM_CLRBREAK    6
+
+#define SCM_MS_CTS_ON   0x01
+#define SCM_MS_DSR_ON   0x02
+#define SCM_MS_RING_ON  0x04
+#define SCM_MS_RLSD_ON  0x08
+
+#define SCM_EV_RXCHAR   0x0001
+#define SCM_EV_RXFLAG   0x0002
+#define SCM_EV_TXEMPTY  0x0004
+#define SCM_EV_CTS      0x0008
+#define SCM_EV_DSR      0x0010
+#define SCM_EV_RLSD     0x0020
+#define SCM_EV_BREAK    0x0040
+#define SCM_EV_ERR      0x0080
+#define SCM_EV_RING     0x0100
+#define SCM_EV_PERR     0x0200
+#define SCM_EV_RX80FULL 0x0400
+#define SCM_EV_EVENT1   0x0800
+#define SCM_EV_EVENT2   0x1000
+
+#define SCM_PURGE_TXABORT  0x0001
+#define SCM_PURGE_RXABORT  0x0002
+#define SCM_PURGE_TXCLEAR  0x0004
+#define SCM_PURGE_RXCLEAR  0x0008
+
+#define SCM_CE_RXOVER   0x0001
+#define SCM_CE_OVERRUN  0x0002
+#define SCM_CE_RXPARITY 0x0004
+#define SCM_CE_FRAME    0x0008
+#define SCM_CE_BREAK    0x0010
+#define SCM_CE_TXFULL   0x0100
+#define SCM_CE_PTO      0x0200
+#define SCM_CE_IOE      0x0400
+#define SCM_CE_DNS      0x0800
+#define SCM_CE_OOP      0x1000
+#define SCM_CE_MODE     0x8000
+
+/* Constant string defines */
+#define FAILTHOWEXP "JNI call ThrowNew failed to throw exception"
+#define SCOMEXPCLASS "com/embeddedunveiled/serial/SerialComException"
+#define JAVALSTRING "java/lang/String"
+#define E_ENBLPARCHKSTR "Parity checking in configureComPortData method needs to be enabled first !"
+#define E_GETJVMSTR "JNI call GetJavaVM failed !"
+#define E_FINDCLASSSCOMEXPSTR "Can not find class com/embeddedunveiled/serial/SerialComException, Probably out of memory."
+#define E_FINDCLASSSSTRINGSTR "Can not find class java/lang/String, Probably out of memory !"
+#define E_NEWOBJECTARRAYSTR "JNI call NewObjectArray failed. Probably out of memory !"
+#define E_NEWBYTEARRAYSTR "JNI call NewByteArray failed !"
+#define E_NEWINTARRAYSTR "JNI call NewIntArray failed !"
+#define E_NEWLONGARRAYSTR "JNI call NewLongArray failed !"
+#define E_SETOBJECTARRAYSTR "JNI call SetObjectArrayElement failed. Either index violation or wrong class used !"
+#define E_SETBYTEARRREGIONSTR "JNI call SetByteArrayRegion failed. Probably index out of bound !"
+#define E_SETINTARRREGIONSTR "JNI call SetIntArrayRegion failed. Probably index out of bound !"
+#define E_SETLONGARRREGIONSTR "JNI call SetLongArrayRegion failed. Probably index out of bound !"
+#define E_SETCHARARRREGIONSTR "JNI call SetCharArrayRegion failed. Probably index out of bound !"
+#define E_NEWSTRUTFSTR "JNI call NewStringUTF failed !"
+#define E_GETSTRUTFCHARSTR "JNI call GetStringUTFChars failed !"
+#define E_GETBYTEARRELEMTSTR "JNI call GetByteArrayElements failed !"
+#define E_GETBYTEARRREGIONSTR "JNI call GetByteArrayRegion failed !"
+#define E_GETINTARRELEMTSTR "JNI call GetIntArrayElements failed !"
+#define E_NEWGLOBALREFSTR "JNI Call NewGlobalRef failed !"
+#define E_DELGLOBALREFSTR "JNI Call DeleteGlobalRef failed !"
+#define E_CALLOCSTR "calloc() failed to allocate requested memory !"
+#define E_REALLOCSTR "realloc() failed to allocate requested memory !"
+#define E_ATTACHCURRENTTHREADSTR "JNI call AttachCurrentThread failed !"
+#define E_GETOBJECTCLASSSTR "JNI call GetObjectClass failed !"
+#define E_GETMETHODIDSTR "JNI call GetMethodID failed !"
+#define E_DETACHCURTHREAD "JNI call DetachCurrentThread failed !"
+#define E_SIGNALINSTFAILSTR "Failed to install signal handler !"
+#define E_CALLVOIDMETHDSTR "JNI call CallVoidMethod failed !"
+#define E_UDEVNEWSTR "Could not create udev context !"
+#define E_UDEVNETLINKSTR "Could not create udev monitor !"
+#define E_IOSRVMATUSBDEVSTR "Function call IOServiceMatching('IOUSBDevice') failed !"
+#define E_GETDIRCTBUFADDRSTR "JNI call GetDirectBufferAddress failed !"
+#define E_VIOVNTINVALIDSTR "The length of data supplied exceeds maximum limit !"
+#define E_IllegalARG "Illegal argument !"
+#define E_INVALIDHANDLE "INVALID_HANDLE_VALUE !"
+
+#define E_SETRTS "Could not set RTS line !"
+#define E_CLRRTS "Could not clear RTS line !"
+#define E_SETDTR "Could not set DTR line !"
+#define E_CLRDTR "Could not clear DTR line !"
+#define E_SETBREAK "Could not set line break !"
+#define E_CLRBREAK "Could not clear line break !"
+#define E_GETMODEMSTATUS "Could not get modem status !"
+#define E_SETUPCOMM "Could not set the input/output buffer size !"
+#define E_SETUPCOMMSTATE "Could not set the state from DCB structure !"
+#define E_GETUPCOMMSTATE "Could not get the DCB structure !"
+#define E_SETCOMMTIMEOUTS "Could not set the timeout values !"
+#define E_GETCOMMTIMEOUTS "Could not get the timeout values !"
+#define E_SETCOMMBREAK "Could not put the communications line in the BREAK state !"
+#define E_CLEARCOMMBREAK "Could not put the communications line in non-BREAK state !"
+#define E_SETCOMMMASK "Could not set given event mask !"
+#define E_GETCOMMMASK "Could not get the event mask !"
+#define E_PURGECOMM "Could not purge the port !"
+#define E_GETLASTERROR "Could not get the last error value !"
+#define E_CLEARCOMMERROR "Could not get the error and status information !"
+#define E_W32WRITEFILE "Could not write given data to device !"
+#define E_W32READFILE "Could not write given data to device !"
+#define E_WAITCOMEVENT "Could not wait for an event to occur !"
+
+
+/* This holds information for implementing dynamically growing array in C language. */
+struct jstrarray_list {
+	jstring *base;      /* pointer to an array of pointers to string */
+	int index;         /* array element index                       */
+	int current_size;  /* size of this array                        */
+};
+
+/* function prototypes (declared in reverse order of use) */
+extern int LOGE(const char *error_msg);
+extern void throw_serialcom_exception(JNIEnv *env, int type, int error_code, const char *);
+extern void free_jstrarraylist(struct jstrarray_list *al);
+extern void insert_jstrarraylist(struct jstrarray_list *al, jstring element);
+extern void init_jstrarraylist(struct jstrarray_list *al, int initial_size);
+
+#endif /* SCM_FTDI_D2XX_LIB_H_ */
---
+++ vendor-libs/ftdi/d2xx/d2xx-mac/src/scm_ftdi_d2xx_lib.c
@@ -0,0 +1,163 @@
+/***************************************************************************************************
+ * Author : Rishi Gupta
+ *
+ * This file is part of 'serial communication manager' library.
+ *
+ * The 'serial communication manager' is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * The 'serial communication manager' is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with serial communication manager. If not, see <http://www.gnu.org/licenses/>.
+ *
+ ***************************************************************************************************/
+
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+#include "scm_ftdi_d2xx.h"
+
+/* Allocate memory of given size and initializes elements as appropriate.
+ * The elements in this array list will be java.lang.String object constructed
+ * from an array of characters in modified UTF-8 encoding by calling JNI
+ * NewStringUTF(..) function. */
+void init_jstrarraylist(struct jstrarray_list *al, int initial_size) {
+	al->base = (jstring *) calloc(initial_size, sizeof(jstring));
+	if(al->base == NULL) {
+		fprintf(stderr, "array calloc %s %d\n", "failed : ", errno);
+		fflush(stderr);
+	}
+	al->index = 0;
+	al->current_size = initial_size;
+}
+
+/* Insert given jstring object reference at next position expanding memory size
+ * allocated if required. */
+void insert_jstrarraylist(struct jstrarray_list *al, jstring element) {
+	if(al->index >= al->current_size) {
+		al->current_size = al->current_size * 2;
+		al->base = (jstring *) realloc(al->base, al->current_size * sizeof(jstring));
+		if(al->base == NULL) {
+			fprintf(stderr, "array realloc %s %d\n", "failed : ", errno);
+			fflush(stderr);
+		}
+	}
+	al->base[al->index] = element;
+	al->index++;
+}
+
+/* Java garbage collector is responsible for releasing memory occupied by jstring objects.
+ * We just free memory that we allocated explicitly. */
+void free_jstrarraylist(struct jstrarray_list *al) {
+	free(al->base);
+}
+
+/*
+ * For C-standard/POSIX/OS specific/Custom/JNI errors, this function is called. It sets a pointer which is checked
+ * by java method when native function returns. If the pointer is set exception of class as set by this function is
+ * thrown.
+ *
+ * The type 1 indicates standard (C-standard/POSIX/OS specific) error, 2 indicate custom (defined by this library)
+ * error, 3 indicates custom error with message string.
+ */
+void throw_serialcom_exception(JNIEnv *env, int type, int error_code, const char *msg) {
+	jint ret = 0;
+	char buffer[256];
+	char *custom_error_msg = NULL;
+	jclass serialComExceptionClass = NULL;
+#if _POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600 && ! _GNU_SOURCE
+#else
+	char *error_msg = NULL;
+#endif
+	(*env)->ExceptionClear(env);
+	serialComExceptionClass = (*env)->FindClass(env, SCOMEXPCLASS);
+	if((serialComExceptionClass == NULL) || ((*env)->ExceptionOccurred(env) != NULL)) {
+		(*env)->ExceptionClear(env);
+		LOGE(E_FINDCLASSSCOMEXPSTR);
+		return;
+	}
+
+	if(type == 1) {
+		/* Caller has given posix/os-standard error code, get error message corresponding to this code. */
+		/* This need to be made more portable to remove compiler specific dependency */
+#if _POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600 && ! _GNU_SOURCE
+		strerror_r(error_code, buffer, 256);
+		ret = (*env)->ThrowNew(env, serialComExceptionClass, buffer);
+		if(ret < 0) {
+			LOGE(FAILTHOWEXP);
+		}
+#else
+		error_msg = strerror_r(error_code, buffer, 256);
+		if(error_msg == NULL) {
+			ret = (*env)->ThrowNew(env, serialComExceptionClass, buffer);
+			if(ret < 0) {
+				LOGE(FAILTHOWEXP);
+			}
+		}else {
+			ret = (*env)->ThrowNew(env, serialComExceptionClass, error_msg);
+			if(ret < 0) {
+				LOGE(FAILTHOWEXP);
+			}
+		}
+#endif
+	}else if(type == 2) {
+		/* Caller has given custom error code, need to get exception message corresponding to this code. */
+		switch (error_code) {
+			case FT_INVALID_HANDLE: custom_error_msg = "FT_INVALID_HANDLE";
+				break;
+			case FT_DEVICE_NOT_FOUND: custom_error_msg = "FT_DEVICE_NOT_FOUND";
+				break;
+			case FT_DEVICE_NOT_OPENED: custom_error_msg = "FT_DEVICE_NOT_OPENED";
+				break;
+			case FT_IO_ERROR: custom_error_msg = "FT_IO_ERROR";
+				break;
+			case FT_INSUFFICIENT_RESOURCES: custom_error_msg = "FT_INSUFFICIENT_RESOURCES";
+				break;
+			case FT_INVALID_PARAMETER: custom_error_msg = "FT_INVALID_PARAMETER";
+				break;
+			case FT_INVALID_BAUD_RATE: custom_error_msg = "FT_INVALID_BAUD_RATE";
+				break;
+			case FT_DEVICE_NOT_OPENED_FOR_ERASE: custom_error_msg = "FT_DEVICE_NOT_OPENED_FOR_ERASE";
+				break;
+			case FT_DEVICE_NOT_OPENED_FOR_WRITE: custom_error_msg = "FT_DEVICE_NOT_OPENED_FOR_WRITE";
+				break;
+			case FT_FAILED_TO_WRITE_DEVICE: custom_error_msg = "FT_FAILED_TO_WRITE_DEVICE";
+				break;
+			case FT_EEPROM_READ_FAILED: custom_error_msg = "FT_EEPROM_READ_FAILED";
+				break;
+			case FT_EEPROM_WRITE_FAILED: custom_error_msg = "FT_EEPROM_WRITE_FAILED";
+				break;
+			case FT_EEPROM_ERASE_FAILED: custom_error_msg = "FT_EEPROM_ERASE_FAILED";
+				break;
+			case FT_EEPROM_NOT_PRESENT: custom_error_msg = "FT_EEPROM_NOT_PRESENT";
+				break;
+			case FT_EEPROM_NOT_PROGRAMMED: custom_error_msg = "FT_EEPROM_NOT_PROGRAMMED";
+				break;
+			case FT_INVALID_ARGS: custom_error_msg = "FT_INVALID_ARGS";
+				break;
+			case FT_NOT_SUPPORTED: custom_error_msg = "FT_NOT_SUPPORTED";
+				break;
+			case FT_OTHER_ERROR: custom_error_msg = "FT_OTHER_ERROR";
+				break;
+			case FT_DEVICE_LIST_NOT_READY: custom_error_msg = "FT_DEVICE_LIST_NOT_READY";
+				break;
+			default : custom_error_msg = "Unknown error occurred !";
+		}
+		ret = (*env)->ThrowNew(env, serialComExceptionClass, custom_error_msg);
+		if(ret < 0) {
+			LOGE(FAILTHOWEXP);
+		}
+	}else {
+		/* Caller has given exception message explicitly */
+		ret = (*env)->ThrowNew(env, serialComExceptionClass, msg);
+		if(ret < 0) {
+			LOGE(FAILTHOWEXP);
+		}
+	}
+}
