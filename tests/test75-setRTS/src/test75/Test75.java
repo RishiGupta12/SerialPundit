e4593cc92ed0fa946fc64eb15dad19813aee367f
Rishi Gupta12 <gupt21@gmail.com>
2015-09-25 11:37:03 +0530

    add manual test for RTS and DTR signals

---
+++ tests/test75-setRTS/src/test75/Test75.java
@@ -0,0 +1,65 @@
+/**
+ * Author : Rishi Gupta
+ * 
+ * This file is part of 'serial communication manager' library.
+ *
+ * The 'serial communication manager' is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * The 'serial communication manager' is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with serial communication manager. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+package test75;
+
+import com.embeddedunveiled.serial.SerialComManager;
+import com.embeddedunveiled.serial.SerialComManager.BAUDRATE;
+import com.embeddedunveiled.serial.SerialComManager.DATABITS;
+import com.embeddedunveiled.serial.SerialComManager.FLOWCONTROL;
+import com.embeddedunveiled.serial.SerialComManager.PARITY;
+import com.embeddedunveiled.serial.SerialComManager.STOPBITS;
+
+// use digital multi meter or oscilloscope to test this
+
+public class Test75 {
+	public static void main(String[] args) {
+		try {
+			SerialComManager scm = new SerialComManager();
+
+			String PORT = null;
+			int osType = scm.getOSType();
+			if(osType == SerialComManager.OS_LINUX) {
+				PORT = "/dev/ttyUSB0";
+			}else if(osType == SerialComManager.OS_WINDOWS) {
+				PORT = "COM51";
+			}else if(osType == SerialComManager.OS_MAC_OS_X) {
+				PORT = "/dev/cu.usbserial-A70362A3";
+			}else if(osType == SerialComManager.OS_SOLARIS) {
+				PORT = null;
+			}else{
+			}
+
+			long handle = scm.openComPort(PORT, true, true, true);
+			scm.configureComPortData(handle, DATABITS.DB_8, STOPBITS.SB_1, PARITY.P_NONE, BAUDRATE.B115200, 0);
+			scm.configureComPortControl(handle, FLOWCONTROL.NONE, 'x', 'x', false, false);
+			
+			int x = 0;
+			for(x=0; x<5; x++) {
+				scm.setRTS(handle, true);
+				Thread.sleep(1000);
+				scm.setRTS(handle, false);
+				Thread.sleep(1000);
+			}
+
+			scm.closeComPort(handle);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
