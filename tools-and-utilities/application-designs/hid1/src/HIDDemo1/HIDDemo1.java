490c140e2e781e968287f3ae6a69c3421cab7abf
Rishi Gupta12 <gupt21@gmail.com>
2015-12-14 13:16:57 +0530

    implement robust HID application in java design using threads

--- tools-and-utilities/application-designs/hid1/src/HIDDemo1/HIDDemo1.java
+++ tools-and-utilities/application-designs/hid1/src/HIDDemo1/HIDDemo1.java
@@ -17,6 +17,9 @@
 
 package HIDDemo1;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
 import com.embeddedunveiled.serial.ISerialComUSBHotPlugListener;
 import com.embeddedunveiled.serial.SerialComException;
 import com.embeddedunveiled.serial.SerialComManager;
@@ -26,80 +29,101 @@ import com.embeddedunveiled.serial.hid.SerialComRawHID;
 import com.embeddedunveiled.serial.usb.SerialComUSB;
 import com.embeddedunveiled.serial.usb.SerialComUSBHID;
 
-/* This class listens for device hot plug events */
-final class HotPlugDeviceWatcher extends HIDApplication1 implements ISerialComUSBHotPlugListener {
+final class USBhotplugConcreteEventHandler extends HIDApplication1 implements Runnable {
+	int event = 0;
 
 	@Override
-	public void onUSBHotPlugEvent(int event) {
+	public void run() {
+		while (true) {
+			try {
+				// until next event comes this will block
+				event = mUSBhotplugEventQueue.take();
 
-		if(event == SerialComUSB.DEV_ADDED) {
-			/* USB HID device is added to system */
+				if (event == SerialComUSB.DEV_ADDED) {
 
-			// User inserted HID device into system. If the HID device is not opened, then 
-			// open it. If it is opened do nothing.
-			if(hidAlreadyOpened != true) {
-				try {
-					// find and get information about our USB HID device
-					SerialComHIDdevice[] usbHidDevicesPresent = scuh.listUSBHIDdevicesWithInfo(MY_VID);
+					/* USB HID device is added to system */
 
-					// loop over the list and match our VID and PID.
-					for(int a=0; a < usbHidDevicesPresent.length; a++) {
-						if((usbHidDevicesPresent[a].getVendorID() == MY_VID) && (usbHidDevicesPresent[a].getProductID() == MY_PID)) {
-							// get the device node/path assigned by operating system to this HID device
-							hidDevNode = usbHidDevicesPresent[a].getDeviceNode();
+					/* Give some time to operating system so that device nodes
+					 * gets created and is assigned correct permissions etc.*/
+					Thread.sleep(500);
+
+					// User inserted HID device into system. If the HID device
+					// is not opened, then
+					// open it. If it is opened do nothing.
+					if (hidAlreadyOpened != true) {
+						deviceRemoved = false;
+						try {
+							// find and get information about our USB HID device
+							SerialComHIDdevice[] usbHidDevicesPresent = scuh.listUSBHIDdevicesWithInfo(MY_VID);
+
+							// loop over the list and match our VID and PID.
+							for (int a = 0; a < usbHidDevicesPresent.length; a++) {
+								if ((usbHidDevicesPresent[a].getVendorID() == MY_VID)
+										&& (usbHidDevicesPresent[a].getProductID() == MY_PID)) {
+									// get the device node/path assigned by
+									// operating system to this HID device
+									hidDevNode = usbHidDevicesPresent[a].getDeviceNode();
+								}
+							}
+
+							// open HID device
+							hidDevHandle = scrh.openHidDeviceR(hidDevNode, true);
+							hidAlreadyOpened = true;
+
+							// Create and start thread that will read data from
+							// HID device
+							// create context for blocking HID I/O operations
+							context = scrh.createBlockingHIDIOContextR();
+							dataReaderThread = new Thread(new HIDInputReportReader());
+							dataReaderThread.start();
+						} catch (Exception e) {
+							e.printStackTrace();
 						}
+					} else {
+						// do nothing
 					}
 
-					// open HID device
-					Thread.sleep(100); // give some time so that device node gets correct permissions
-					hidDevHandle = scrh.openHidDeviceR(hidDevNode, true);
-					hidAlreadyOpened = true;
+				} else if (event == SerialComUSB.DEV_REMOVED) {
+					/* USB HID device is removed from system */
 
-					// Create and start thread that will read data from HID device
-					dataReaderThread = new Thread(new HIDDataReader());
-					dataReaderThread.start();
-				} catch (Exception e) {
-					e.printStackTrace();
+					// If the HID device is removed, the handle becomes invalid,
+					// so close this handle.
+					// We will open the device again when user plugin the device
+					// in computer again.
+					try {
+						deviceRemoved = true;
+						scrh.unblockBlockingHIDIOOperationR(context);
+						scrh.destroyBlockingIOContextR(context);
+						scrh.closeHidDeviceR(hidDevHandle);
+						hidAlreadyOpened = false;
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+
+				} else {
+					// will not happen but just keep for future
 				}
-			}else {
-				// do nothing
-			}
-
-		}else if(event == SerialComUSB.DEV_REMOVED) {
-			/* USB HID device is removed from system */
-
-			// If the HID device is removed, the handle become invalid, so close this handle.
-			// We will open the device again when user plugin the device in computer again.
-			try {
-				scrh.closeHidDeviceR(hidDevHandle);
-				hidAlreadyOpened = false;
-				deviceRemoved = true;
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-
-		}else {
-			// will not happen but just keep for future
 		}
 	}
 }
 
 /* This class listens for data from USB HID device */
-final class HIDDataReader extends HIDApplication1 implements Runnable {
+final class HIDInputReportReader extends HIDApplication1 implements Runnable {
 	@Override
 	public void run() {
 		try {
-			// create context for blocking HID I/O operations
-			context = scrh.createBlockingHIDIOContextR();
-
 			// keep reading data until this thread is stopped
-			while(true) {
+			while (true) {
 				try {
-					// read data from HID device, it will block if there  is no data
+					// read data from HID device, it will block if there is no data
 					ret = 0;
 					ret = scrh.readInputReportR(hidDevHandle, inputReportBuffer, context);
 				} catch (SerialComException e1) {
-					if(SerialComHID.EXP_UNBLOCK_HIDIO.equals(e1.getExceptionMsg())) {
+					if (SerialComHID.EXP_UNBLOCK_HIDIO.equals(e1
+							.getExceptionMsg())) {
 						// this thread should exit as other thread told it to return
 						return;
 					}
@@ -107,17 +131,17 @@ final class HIDDataReader extends HIDApplication1 implements Runnable {
 					e2.printStackTrace();
 				}
 
-				// user removed HID device from system, eit this thread.
-				// it will get created automatically when user insert device
-				// in system again.
-				if(deviceRemoved == true) {
+				// user removed HID device from system, exit this thread.
+				// it will get created automatically when user insert device in system again.
+				if (deviceRemoved == true) {
+					System.out.println("Did you removed device from system ? Please plug-in again to resume operations !");
 					return;
 				}
 
-				// print data if read from HID device actually, readInputReportR method may return 
-				// prematurally if user removed device from system while readInputReportR was 
+				// print data if read from HID device actually, readInputReportR method may return
+				// pre-maturally if user removed device from system while readInputReportR was
 				// blocked to read it.
-				if(ret > 0) {
+				if (ret > 0) {
 					System.out.println(scrh.formatReportToHexR(inputReportBuffer, " "));
 				}
 			}
@@ -127,10 +151,11 @@ final class HIDDataReader extends HIDApplication1 implements Runnable {
 	}
 }
 
-class HIDApplication1 {
+class HIDApplication1 implements ISerialComUSBHotPlugListener {
 
 	/* ********************************************************* */
 	// Replace MY_VID value and MY_PID value with your USB-IF VID and PID.
+	// this VID/PID is for MCP2200.
 	protected static int MY_VID = 0x04d8;
 	protected static int MY_PID = 0x00df;
 	/* ********************************************************* */
@@ -140,18 +165,22 @@ class HIDApplication1 {
 	protected static SerialComUSBHID scuh;
 	protected static int hothandle;
 	protected static long hidDevHandle;
-	protected static HotPlugDeviceWatcher hpdw;
 	protected static boolean hidAlreadyOpened;
 	protected static String hidDevNode;
 	protected static long context = 0;
 	protected static int ret = 0;
 	protected static Thread dataReaderThread = null;
+	protected static Thread hotPlugHandlerThread = null;
 	protected static boolean deviceRemoved = false;
+	protected static Object lock = new Object();
+	protected static BlockingQueue<Integer> mUSBhotplugEventQueue = new ArrayBlockingQueue<Integer>(
+			1024);
 
 	/* ************ */
 	// Set buffer size as per your HID device, this example id for MCP2200.
 	protected static byte[] inputReportBuffer = new byte[16];
 	protected static byte[] outputReportBuffer = new byte[16];
+
 	/* ************ */
 
 	public void begin() {
@@ -161,16 +190,17 @@ class HIDApplication1 {
 			scrh = (SerialComRawHID) scm.getSerialComHIDInstance(SerialComHID.MODE_RAW, null, null);
 			scuh = (SerialComUSBHID) scrh.getHIDTransportInstance(SerialComHID.HID_USB);
 
-			if(scm.isUSBDevConnected(MY_VID, MY_PID, null)) {
+			if (scm.isUSBDevConnected(MY_VID, MY_PID, null)) {
 
 				// find and get information about our USB HID device
 				SerialComHIDdevice[] usbHidDevicesPresent = scuh.listUSBHIDdevicesWithInfo(MY_VID);
 
 				// loop over the list and match our VID and PID.
-				for(int a=0; a < usbHidDevicesPresent.length; a++) {
-					if((usbHidDevicesPresent[a].getVendorID() == MY_VID) && (usbHidDevicesPresent[a].getProductID() == MY_PID)) {
-						// get the device node/path assigned by operating system to 
-						// this HID device
+				for (int a = 0; a < usbHidDevicesPresent.length; a++) {
+					if ((usbHidDevicesPresent[a].getVendorID() == MY_VID)
+							&& (usbHidDevicesPresent[a].getProductID() == MY_PID)) {
+						// get the device node/path assigned by operating system
+						// to this HID device
 						hidDevNode = usbHidDevicesPresent[a].getDeviceNode();
 					}
 				}
@@ -179,46 +209,68 @@ class HIDApplication1 {
 				hidDevHandle = scrh.openHidDeviceR(hidDevNode, true);
 				hidAlreadyOpened = true;
 
-				// Create listener object and register it so that whenever HID device is removed from system, 
-				// application gets notification.
-				hpdw = new HotPlugDeviceWatcher();
-				hothandle = scm.registerUSBHotPlugEventListener(hpdw, MY_VID, MY_PID, null);
+				// before registering hot plug listener start thread
+				hotPlugHandlerThread = new Thread(new USBhotplugConcreteEventHandler());
+				hotPlugHandlerThread.start();
 
 				// Create and start thread that will read data from HID device
-				dataReaderThread = new Thread(new HIDDataReader());
+				// create context for blocking HID I/O operations
+				context = scrh.createBlockingHIDIOContextR();
+				dataReaderThread = new Thread(new HIDInputReportReader());
 				dataReaderThread.start();
-			}else {
-				// Create listener object and register it. Whenever our HID device will be connected, 
-				// listener will be called. HID device is then opened from listener.
+
+				// register usb hot plug listener.
+				hothandle = scm.registerUSBHotPlugEventListener(this, MY_VID, MY_PID, null);
+			} else {
+				// before registering hot plug listener start thread
+				hotPlugHandlerThread = new Thread(new USBhotplugConcreteEventHandler());
+				hotPlugHandlerThread.start();
+
+				// register hot plug listener. HID device is then opened from
+				// listener.
 				hidAlreadyOpened = false;
-				hpdw = new HotPlugDeviceWatcher();
-				hothandle = scm.registerUSBHotPlugEventListener(hpdw, MY_VID, MY_PID, null);
+				hothandle = scm.registerUSBHotPlugEventListener(this, MY_VID, MY_PID, null);
 			}
 
-			/* ************* YOUR LOGIC may be written in this block ********************** */
-			// write your application logic here; do rest of things you want to do. 
-			// 1. In this example, this applications sends a command to MCP2200 and sleeps. MCP2200 send 
-			//    response which is printed by reader thread.
-			// 2. On application exit call scm.unregisterUSBHotPlugEventListener(hothandle);
-			// 3. On application exit call scrh.unblockBlockingHIDIOOperationR(context) and then call
-			//    scrh.destroyBlockingIOContextR(context).
+			/* ************* YOUR application logic goes here ************************/
+			// write your application logic here; do rest of things you want to
+			// do.
+			// 1. In this example, this applications sends a command to MCP2200
+			// and sleeps. MCP2200 send
+			// response which is printed by reader thread.
+			// 2. On application exit call
+			// scm.unregisterUSBHotPlugEventListener(hothandle);
+			// 3. On application exit call
+			// scrh.unblockBlockingHIDIOOperationR(context) and then call
+			// scrh.destroyBlockingIOContextR(context).
 			// 4. On application exit call scrh.closeHidDeviceR(hidDevHandle);
-			try {
-				while(true) {
+			while (true) {
+				try {
 					Thread.sleep(1500);
-					if(hidAlreadyOpened == true) {
+					if (hidAlreadyOpened == true) {
 						outputReportBuffer[0] = (byte) 0x80;
 						ret = scrh.writeOutputReportR(hidDevHandle, (byte) -1, outputReportBuffer);
 					}
+				} catch (Exception e) {
+					System.out.println("Did you removed device from system ? Please plug-in again to resume operations !");
 				}
-			} catch (Exception e) {
-				e.printStackTrace();
 			}
-			/* **************************************************************************** */
+			/* **********************************************************************/
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	/*
+	 * This method must return as soon as possible. All the steps that must be
+	 * executed should be executed in another thread. Here we notify other
+	 * thread that a USB hot plug event has occured and pass that thread event
+	 * value in-directly.
+	 */
+	@Override
+	public void onUSBHotPlugEvent(int event) {
+		mUSBhotplugEventQueue.offer(event);
 	}
 }
 
