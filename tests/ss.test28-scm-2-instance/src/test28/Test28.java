c35a14054d606800b463e5dbf8421fbfaa81c6a6
Rishi Gupta12 <gupt21@gmail.com>
2016-03-14 18:16:21 +0530

    add test for new serialcomamanager constructor

--- tests/ss.test28-scm-2-instance/src/test28/Test28.java
+++ tests/ss.test28-scm-2-instance/src/test28/Test28.java
@@ -32,11 +32,11 @@ public class Test28 {
 	public static void main(String[] args) {
 		long handle1 = 0;
 		long handle = 0;
-		
+
 		try {
 			SerialComManager scm = new SerialComManager();
 			SerialComManager scm1 = new SerialComManager();
-/*
+
 			String PORT = null;
 			String PORT1 = null;
 			int osType = scm.getOSType();
@@ -58,7 +58,7 @@ public class Test28 {
 			handle = scm.openComPort(PORT, true, true, true);
 			scm.configureComPortData(handle, DATABITS.DB_8, STOPBITS.SB_1, PARITY.P_NONE, BAUDRATE.B9600, 0);
 			scm.configureComPortControl(handle, FLOWCONTROL.NONE, 'x', 'x', false, false);
-	
+
 			handle1 = scm1.openComPort(PORT1, true, true, true);
 			scm1.configureComPortData(handle1, DATABITS.DB_8, STOPBITS.SB_1, PARITY.P_NONE, BAUDRATE.B9600, 0);
 			scm1.configureComPortControl(handle1, FLOWCONTROL.NONE, 'x', 'x', false, false);
@@ -68,7 +68,45 @@ public class Test28 {
 			System.out.println("data read is : " + scm.readString(handle));
 			System.out.println("data1 read is : " + scm1.readString(handle1));
 			System.out.println("close status : " + scm.closeComPort(handle));
-			System.out.println("close status : " + scm1.closeComPort(handle1)); */
+			System.out.println("close status : " + scm1.closeComPort(handle1));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			SerialComManager scm = new SerialComManager("scmlib", "/home/r/ab/fg", true);
+			String PORT = null;
+			String PORT1 = null;
+			int osType = scm.getOSType();
+			if(osType == SerialComManager.OS_LINUX) {
+				PORT = "/dev/ttyUSB0";
+				PORT1 = "/dev/ttyUSB1";
+			}else if(osType == SerialComManager.OS_WINDOWS) {
+				PORT = "COM51";
+				PORT1 = "COM52";
+			}else if(osType == SerialComManager.OS_MAC_OS_X) {
+				PORT = "/dev/cu.usbserial-A70362A3";
+				PORT1 = "/dev/cu.usbserial-A602RDCH";
+			}else if(osType == SerialComManager.OS_SOLARIS) {
+				PORT = null;
+				PORT1 = null;
+			}else{
+			}
+
+			handle = scm.openComPort(PORT, true, true, true);
+			scm.configureComPortData(handle, DATABITS.DB_8, STOPBITS.SB_1, PARITY.P_NONE, BAUDRATE.B9600, 0);
+			scm.configureComPortControl(handle, FLOWCONTROL.NONE, 'x', 'x', false, false);
+
+			handle1 = scm.openComPort(PORT1, true, true, true);
+			scm.configureComPortData(handle1, DATABITS.DB_8, STOPBITS.SB_1, PARITY.P_NONE, BAUDRATE.B9600, 0);
+			scm.configureComPortControl(handle1, FLOWCONTROL.NONE, 'x', 'x', false, false);
+
+			scm.writeString(handle, "testing4", 0);
+			scm.writeString(handle1, "testing3", 0);
+			System.out.println("data read is : " + scm.readString(handle));
+			System.out.println("data1 read is : " + scm.readString(handle1));
+			System.out.println("close status : " + scm.closeComPort(handle));
+			System.out.println("close status : " + scm.closeComPort(handle1));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
