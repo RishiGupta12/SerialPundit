21a83bc7a93e8abbc8c0d035482ae8763a3b31b3
Rishi Gupta12 <gupt21@gmail.com>
2016-05-11 18:27:14 +0530

    implement blocking byte stream API

--- com.embeddedunveiled.serial/src/com/embeddedunveiled/serial/SerialComOutByteStream.java
+++ com.embeddedunveiled.serial/src/com/embeddedunveiled/serial/SerialComOutByteStream.java
@@ -32,144 +32,213 @@ import com.embeddedunveiled.serial.internal.SerialComPortHandleInfo;
  */
 public final class SerialComOutByteStream extends OutputStream implements ISerialIOStream {
 
-	private SerialComManager scm;
-	private SerialComPortHandleInfo portHandleInfo;
-	private long handle;
-	private boolean isOpened;
-	/* private boolean isBlocking = false; */
+    private final SerialComManager scm;
+    private final SerialComPortHandleInfo portHandleInfo;
+    private final long handle;
+    private final Object lock;
+    private final boolean isBlocking;
+    private final long context;
+    private boolean isOpened;
 
-	/**
-	 * <p>Allocates a new SerialComOutByteStream object.</p>
-	 * 
-	 * @param scm instance of SerialComManager class with which this stream will associate itself.
-	 * @param handle handle of the serial port on which to write data bytes.
-	 * @param streamMode indicates blocking or non-blocking behavior of stream.
-	 * @throws SerialComException if serial port can not be configured for specified write behavior.
-	 */
-	public SerialComOutByteStream(SerialComManager scm, SerialComPortHandleInfo portHandleInfo, long handle, 
-			SMODE streamMode) throws SerialComException {
-		this.scm = scm;
-		this.portHandleInfo = portHandleInfo;
-		this.handle = handle;
-		isOpened = true;
-		/* if(streamMode.getValue() == 1) {
-			isBlocking = true;
-		} */
-	}
+    /**
+     * <p>Allocates a new SerialComOutByteStream object.</p>
+     * 
+     * @param scm instance of SerialComManager class with which this stream will associate itself.
+     * @param handle handle of the serial port on which to write data bytes.
+     * @param streamMode indicates blocking or non-blocking behavior of stream.
+     * @throws SerialComException if serial port can not be configured for specified write behavior.
+     */
+    public SerialComOutByteStream(SerialComManager scm, SerialComPortHandleInfo portHandleInfo, long handle, 
+            SMODE streamMode) throws SerialComException {
 
-	/**
-	 * <p>Writes the specified byte to this output stream (eight low-order bits of the argument data).
-	 * The 24 high-order bits of data are ignored.</p>
-	 * 
-	 * @param data integer to be written to serial port.
-	 * @throws IOException if write fails or output stream has been closed.
-	 */
-	@Override
-	public void write(int data) throws IOException {
-		if(isOpened != true) {
-			throw new IOException("The byte stream has been closed !");
-		}
-		try {
-			scm.writeSingleByte(handle, (byte)data);
-		} catch (SerialComException e) {
-			throw new IOException(e.getExceptionMsg());
-		}
-	}
+        this.scm = scm;
+        this.portHandleInfo = portHandleInfo;
+        this.handle = handle;
+        lock = new Object();
 
-	/**
-	 * <p>Writes data.length bytes from the specified byte array to this output stream.</p>
-	 * 
-	 * @param data byte type array of data to be written to serial port.
-	 * @throws IOException if write fails or output stream has been closed.
-	 * @throws NullPointerException if data is null.
-	 * @throws IllegalArgumentException if data is not a byte type array.
-	 */
-	@Override
-	public void write(byte[] data) throws IOException {
-		if(isOpened != true) {
-			throw new IOException("The byte stream has been closed !");
-		}
-		if(data == null) {
-			throw new NullPointerException("Argument data can not be null !");
-		}
-		if(!(data instanceof byte[])) {
-			throw new IllegalArgumentException("Argument data is not byte type array !");
-		}
-		try {
-			scm.writeBytes(handle, data, 0);
-		} catch (SerialComException e) {
-			throw new IOException(e.getExceptionMsg());
-		}
-	}
+        if(streamMode.getValue() == 1) {
+            context = scm.createBlockingIOContext();
+            isBlocking = true;
+        }else {
+            context = 0;
+            isBlocking = false;
+        }
+        isOpened = true;
+    }
 
-	/**
-	 * <p>Writes len bytes from the specified byte array starting at offset off to this output stream.</p>
-	 * <p>If b is null, a NullPointerException is thrown.</p>
-	 * <p>If off is negative, or len is negative, or off+len is greater than the length of the array data, 
-	 * then an IndexOutOfBoundsException is thrown.<p>
-	 * 
-	 * @param data byte type array of data to be written to serial port.
-	 * @param off offset from where to start sending data.
-	 * @param len length of data to be written.
-	 * @throws IOException if write fails or output stream has been closed.
-	 * @throws IllegalArgumentException if data is not a byte type array.
-	 * @throws NullPointerException if data is null.
-	 * @throws IndexOutOfBoundsException If off is negative, or len is negative, or off+len is greater 
-	 * than the length of the array data.
-	 */
-	@Override
-	public void write(byte[] data, int off, int len) throws IOException, IndexOutOfBoundsException {
-		if(isOpened != true) {
-			throw new IOException("The byte stream has been closed !");
-		}
-		if(data == null) {
-			throw new NullPointerException("Argument data can not be null !");
-		}
-		if((off < 0) || (len < 0) || ((off+len) > data.length)) {
-			throw new IndexOutOfBoundsException("Index violation detected !");
-		}
-		if(!(data instanceof byte[])) {
-			throw new IllegalArgumentException("Argument data is not byte type array !");
-		}
+    /**
+     * <p>Writes the specified byte to this output stream (eight low-order bits of the argument data).
+     * The 24 high-order bits of data are ignored.</p>
+     * 
+     * @param data integer to be written to serial port.
+     * @throws IOException if write fails or output stream has been closed.
+     */
+    @Override
+    public void write(int data) throws IOException {
+        if(isOpened != true) {
+            throw new IOException("The byte stream has been closed !");
+        }
+        try {
+            if(isBlocking == true) {
+                synchronized(lock) {
+                    try {
+                        byte[] buffer = new byte[1];
+                        buffer[0] = (byte)data;
+                        data = scm.writeBytesBlocking(handle, buffer, context);
+                    }catch (SerialComException e) {
+                        if(SerialComManager.EXP_UNBLOCKIO.equals(e.getExceptionMsg())) {
+                            // this exception message occurs when application has closed stream.
+                            // release lock so that blocking context can be destroyed.
+                            return;
+                        }
+                        // this is error other than expected, pass it to the application.
+                        throw new IOException(e.getExceptionMsg());
+                    }
+                }
+            }else {
+                int ret = scm.writeSingleByte(handle, (byte)data);
+                if(ret == 0) {
+                    throw new IOException("Given data not sent to serial port. Please retry !");
+                }
+            }
+        } catch (SerialComException e) {
+            throw new IOException(e.getExceptionMsg());
+        }
+    }
 
-		try {
-			int x = 0;
-			int i = off;
-			byte[] buf = new byte[len];
-			for(x=0; x<len; x++) {
-				buf[x] = data[i];
-				i++;
-			}
-			scm.writeBytes(handle, buf, 0);
-		} catch (SerialComException e) {
-			throw new IOException(e.getExceptionMsg());
-		}
-	}
+    /**
+     * <p>Writes data.length bytes from the specified byte array to this output stream.</p>
+     * 
+     * @param data byte type array of data to be written to serial port.
+     * @throws IOException if write fails or output stream has been closed.
+     * @throws IllegalArgumentException if data is null or an empty array.
+     */
+    @Override
+    public void write(byte[] data) throws IOException {
+        if(isOpened != true) {
+            throw new IOException("The byte stream has been closed !");
+        }
+        if((data == null) || (data.length == 0)) {
+            throw new IllegalArgumentException("Argument data can not be null or an empty array !");
+        }
+        try {
+            if(isBlocking == true) {
+                synchronized(lock) {
+                    try {
+                        int result = scm.writeBytesBlocking(handle, data, context);
+                        if(result == 0) {
+                            throw new IOException("Given data not sent to serial port. Please retry !");
+                        }
+                    }catch (SerialComException e) {
+                        if(SerialComManager.EXP_UNBLOCKIO.equals(e.getExceptionMsg())) {
+                            return;
+                        }
+                        throw new IOException(e.getExceptionMsg());
+                    }
+                }
+            }else {
+                int ret = scm.writeBytes(handle, data, 0);
+                if(ret == 0) {
+                    throw new IOException("Given data not sent to serial port. Please retry !");
+                }
+            }
+        } catch (SerialComException e) {
+            throw new IOException(e.getExceptionMsg());
+        }
+    }
 
-	/**
-	 * <p>SCM always flushes data every time writeBytes() method is called. So do nothing just return.</p>
-	 * 
-	 * @throws IOException if write fails or output stream has been closed.
-	 */
-	@Override
-	public void flush() throws IOException {
-		if(isOpened != true) {
-			throw new IOException("The byte stream has been closed !");
-		}
-	}
+    /**
+     * <p>Writes len bytes from the specified byte array starting at offset off to this output stream.</p>
+     * <p>If b is null, a NullPointerException is thrown.</p>
+     * <p>If off is negative, or len is negative, or off+len is greater than the length of the array data, 
+     * then an IndexOutOfBoundsException is thrown.<p>
+     * 
+     * @param data byte type array of data to be written to serial port.
+     * @param off offset from where to start sending data.
+     * @param len length of data to be written.
+     * @throws IOException if write fails or output stream has been closed.
+     * @throws IllegalArgumentException if data is not a byte type array.
+     * @throws NullPointerException if data is null.
+     * @throws IndexOutOfBoundsException If off is negative, or len is negative, or off+len is greater 
+     * than the length of the array data.
+     */
+    @Override
+    public void write(byte[] data, final int off, final int len) throws IOException, IndexOutOfBoundsException {
+        if(isOpened != true) {
+            throw new IOException("The byte stream has been closed !");
+        }
+        if((data == null) || (data.length == 0)) {
+            throw new IllegalArgumentException("Argument data can not be null or an empty array !");
+        }
+        if((off < 0) || (len < 0) || ((off+len) > data.length)) {
+            throw new IndexOutOfBoundsException("Index violation detected in given data array !");
+        }
 
-	/**
-	 * <p>This method releases the OutputStream object internally associated with the operating handle.</p>
-	 * <p>To actually close the port closeComPort() method should be used.</p>
-	 * 
-	 * @throws IOException if write fails or output stream has been closed.
-	 */
-	@Override
-	public void close() throws IOException {
-		if(isOpened != true) {
-			throw new IOException("The byte stream has been closed !");
-		}
-		portHandleInfo.setSerialComOutByteStream(null);
-		isOpened = false;
-	}
+        int i = off;
+        byte[] buf = new byte[len];
+        for(int x=0; x<len; x++) {
+            buf[x] = data[i];
+            i++;
+        }
+        try {
+            if(isBlocking == true) {
+                synchronized(lock) {
+                    try {
+                        int result = scm.writeBytesBlocking(handle, buf, context);
+                        if(result == 0) {
+                            throw new IOException("Given data not sent to serial port. Please retry !");
+                        }
+                    }catch (SerialComException e) {
+                        if(SerialComManager.EXP_UNBLOCKIO.equals(e.getExceptionMsg())) {
+                            return;
+                        }
+                        throw new IOException(e.getExceptionMsg());
+                    }
+                }
+            }else {
+                int ret = scm.writeBytes(handle, buf, 0);
+                if(ret == 0) {
+                    throw new IOException("Given data not sent to serial port. Please retry !");
+                }
+            }
+        } catch (SerialComException e) {
+            throw new IOException(e.getExceptionMsg());
+        }
+    }
+
+    /**
+     * <p>SCM always flushes data every time writeBytes() method is called. So do nothing just return.</p>
+     * 
+     * @throws IOException if write fails or output stream has been closed.
+     */
+    @Override
+    public void flush() throws IOException {
+        if(isOpened != true) {
+            throw new IOException("The byte stream has been closed !");
+        }
+    }
+
+    /**
+     * <p>This method releases the OutputStream object internally associated with the operating handle.</p>
+     * <p>To actually close the port closeComPort() method should be used.</p>
+     * 
+     * @throws IOException if write fails or output stream has been closed.
+     */
+    @Override
+    public void close() throws IOException {
+        if(isOpened != true) {
+            throw new IOException("The byte stream has been already closed !");
+        }
+        if(isBlocking == true) {
+            scm.unblockBlockingIOOperation(context);
+            // if there was a blocked write operation, it will hold this lock. when it gets unblocked
+            // it will release this lock and therefore this close method will acquire this lock.
+            // once the lock is acquired it is safe to destroy context.
+            synchronized(lock) {
+                scm.destroyBlockingIOContext(context);
+            }
+        }
+        isOpened = false;
+        portHandleInfo.setSerialComOutByteStream(null);
+    }
 }
