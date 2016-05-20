334d55e13c400459f7d928e58c459b81417fe155
Rishi Gupta12 <gupt21@gmail.com>
2016-05-20 18:52:22 +0530

    update test for long data listener

--- tests/test19-long-datalistener/src/test19/Test19.java
+++ tests/test19-long-datalistener/src/test19/Test19.java
@@ -27,72 +27,75 @@ import com.embeddedunveiled.serial.SerialComManager.STOPBITS;
 import com.embeddedunveiled.serial.ISerialComDataListener;
 
 public class Test19 implements ISerialComDataListener {
-    
-    int x = 0;
 
-    public static void main(String[] args) {
+	static int x = 0;
 
-        try {
-            SerialComManager scm = new SerialComManager();
+	public static void main(String[] args) {
 
-            String PORT = null;
-            String PORT1 = null;
-            int osType = scm.getOSType();
-            if(osType == SerialComManager.OS_LINUX) {
-                PORT = "/dev/ttyUSB0";
-                PORT1 = "/dev/ttyUSB1";
-            }else if(osType == SerialComManager.OS_WINDOWS) {
-                PORT = "COM51";
-                PORT1 = "COM52";
-            }else if(osType == SerialComManager.OS_MAC_OS_X) {
-                PORT = "/dev/cu.usbserial-A70362A3";
-                PORT1 = "/dev/cu.usbserial-A602RDCH";
-            }else if(osType == SerialComManager.OS_SOLARIS) {
-                PORT = null;
-                PORT1 = null;
-            }else{
-            }
+		try {
+			SerialComManager scm = new SerialComManager();
 
-            PORT = "/dev/pts/2";
-            PORT1 = "/dev/pts/3";
-            
-            // receiver
-            long handle = scm.openComPort(PORT, true, true, true);
-            scm.configureComPortData(handle, DATABITS.DB_8, STOPBITS.SB_1, PARITY.P_NONE, BAUDRATE.B115200, 0);
-            scm.configureComPortControl(handle, FLOWCONTROL.NONE, 'x', 'x', false, false);
+			String PORT = null;
+			String PORT1 = null;
+			int osType = scm.getOSType();
+			if(osType == SerialComManager.OS_LINUX) {
+				PORT = "/dev/ttyUSB0";
+				PORT1 = "/dev/ttyUSB1";
+			}else if(osType == SerialComManager.OS_WINDOWS) {
+				PORT = "COM5";
+				PORT1 = "COM11";
+			}else if(osType == SerialComManager.OS_MAC_OS_X) {
+				PORT = "/dev/cu.usbserial-A70362A3";
+				PORT1 = "/dev/cu.usbserial-A602RDCH";
+			}else if(osType == SerialComManager.OS_SOLARIS) {
+				PORT = null;
+				PORT1 = null;
+			}else{
+			}
 
-            Test19 dataListener = new Test19();
-            System.out.println("registering data listener");
-            scm.registerDataListener(handle, dataListener);
-            System.out.println("registered data listner");
+			// receiver
+			long handle = scm.openComPort(PORT, true, true, true);
+			scm.configureComPortData(handle, DATABITS.DB_8, STOPBITS.SB_1, PARITY.P_NONE, BAUDRATE.B115200, 0);
+			scm.configureComPortControl(handle, FLOWCONTROL.NONE, 'x', 'x', false, false);
 
-            // sender
-            long handle1 = scm.openComPort(PORT1, true, true, true);
-            scm.configureComPortData(handle1, DATABITS.DB_8, STOPBITS.SB_1, PARITY.P_NONE, BAUDRATE.B115200, 0);
-            scm.configureComPortControl(handle1, FLOWCONTROL.NONE, 'x', 'x', false, false);
-            
-            byte[] buffer = new byte[1056];
+			Test19 dataListener = new Test19();
+			System.out.println("registering data listener");
+			scm.registerDataListener(handle, dataListener);
+			System.out.println("registered data listner");
 
-            scm.writeBytes(handle1, buffer, 0);
-            Thread.sleep(2000); // wait for data to be displayed on console
+			// sender
+			long handle1 = scm.openComPort(PORT1, true, true, true);
+			scm.configureComPortData(handle1, DATABITS.DB_8, STOPBITS.SB_1, PARITY.P_NONE, BAUDRATE.B115200, 0);
+			scm.configureComPortControl(handle1, FLOWCONTROL.NONE, 'x', 'x', false, false);
 
-            scm.unregisterDataListener(handle, dataListener);
-            scm.closeComPort(handle);
-            scm.closeComPort(handle1);
-            System.out.println("done");
-        }catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+			byte[] buffer = new byte[1056];
 
-    @Override
-    public void onDataListenerError(int arg0) {
-        System.out.println("data error : " + arg0);
-    }
+			scm.writeBytes(handle1, buffer, 0);
+			Thread.sleep(2000); // wait for data to be displayed on console
 
-    @Override
-    public void onNewSerialDataAvailable(byte[] data) {
-        x = x + data.length;
-        System.out.println("data length till now : " + x);
-    }
+			System.out.println("\n");
+			x = 0;
+
+			scm.writeBytes(handle1, buffer, 0);
+			Thread.sleep(2000); // wait for data to be displayed on console
+
+			scm.unregisterDataListener(handle, dataListener);
+			scm.closeComPort(handle);
+			scm.closeComPort(handle1);
+			System.out.println("done");
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void onDataListenerError(int arg0) {
+		System.out.println("data error : " + arg0);
+	}
+
+	@Override
+	public void onNewSerialDataAvailable(byte[] data) {
+		x = x + data.length;
+		System.out.println("data length till now : " + x);
+	}
 }
