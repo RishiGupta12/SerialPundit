a7381a2671e4e1e0535c4720a1f5362d408112d9
Rishi Gupta12 <gupt21@gmail.com>
2016-05-20 11:59:14 +0530

    add test for data listener

---
+++ tests/test19-long-datalistener/src/test19/Test19.java
@@ -0,0 +1,98 @@
+/*
+ * Author : Rishi Gupta
+ * 
+ * This file is part of 'serial communication manager' library.
+ * Copyright (C) <2014-2016>  <Rishi Gupta>
+ *
+ * This 'serial communication manager' is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * The 'serial communication manager' is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR 
+ * A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with 'serial communication manager'.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package test19;
+
+import com.embeddedunveiled.serial.SerialComManager;
+import com.embeddedunveiled.serial.SerialComManager.BAUDRATE;
+import com.embeddedunveiled.serial.SerialComManager.DATABITS;
+import com.embeddedunveiled.serial.SerialComManager.FLOWCONTROL;
+import com.embeddedunveiled.serial.SerialComManager.PARITY;
+import com.embeddedunveiled.serial.SerialComManager.STOPBITS;
+import com.embeddedunveiled.serial.ISerialComDataListener;
+
+public class Test19 implements ISerialComDataListener {
+    
+    int x = 0;
+
+    public static void main(String[] args) {
+
+        try {
+            SerialComManager scm = new SerialComManager();
+
+            String PORT = null;
+            String PORT1 = null;
+            int osType = scm.getOSType();
+            if(osType == SerialComManager.OS_LINUX) {
+                PORT = "/dev/ttyUSB0";
+                PORT1 = "/dev/ttyUSB1";
+            }else if(osType == SerialComManager.OS_WINDOWS) {
+                PORT = "COM51";
+                PORT1 = "COM52";
+            }else if(osType == SerialComManager.OS_MAC_OS_X) {
+                PORT = "/dev/cu.usbserial-A70362A3";
+                PORT1 = "/dev/cu.usbserial-A602RDCH";
+            }else if(osType == SerialComManager.OS_SOLARIS) {
+                PORT = null;
+                PORT1 = null;
+            }else{
+            }
+
+            PORT = "/dev/pts/2";
+            PORT1 = "/dev/pts/3";
+            
+            // receiver
+            long handle = scm.openComPort(PORT, true, true, true);
+            scm.configureComPortData(handle, DATABITS.DB_8, STOPBITS.SB_1, PARITY.P_NONE, BAUDRATE.B115200, 0);
+            scm.configureComPortControl(handle, FLOWCONTROL.NONE, 'x', 'x', false, false);
+
+            Test19 dataListener = new Test19();
+            System.out.println("registering data listener");
+            scm.registerDataListener(handle, dataListener);
+            System.out.println("registered data listner");
+
+            // sender
+            long handle1 = scm.openComPort(PORT1, true, true, true);
+            scm.configureComPortData(handle1, DATABITS.DB_8, STOPBITS.SB_1, PARITY.P_NONE, BAUDRATE.B115200, 0);
+            scm.configureComPortControl(handle1, FLOWCONTROL.NONE, 'x', 'x', false, false);
+            
+            byte[] buffer = new byte[1056];
+
+            scm.writeBytes(handle1, buffer, 0);
+            Thread.sleep(2000); // wait for data to be displayed on console
+
+            scm.unregisterDataListener(handle, dataListener);
+            scm.closeComPort(handle);
+            scm.closeComPort(handle1);
+            System.out.println("done");
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onDataListenerError(int arg0) {
+        System.out.println("data error : " + arg0);
+    }
+
+    @Override
+    public void onNewSerialDataAvailable(byte[] data) {
+        x = x + data.length;
+        System.out.println("data length till now : " + x);
+    }
+}
