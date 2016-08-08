3f87c300ab0ea2b0986410fea48ea0d6ff965a90
Rishi Gupta12 <gupt21@gmail.com>
2016-08-08 07:23:32 +0530

    add usb reset utility for usb-serial development

--- drivers/tty2comKm/application/src/tty2comKmApp/TTY2COMApp.java
+++ drivers/tty2comKm/application/src/tty2comKmApp/TTY2COMApp.java
@@ -13,11 +13,15 @@
 
 package tty2comKmApp;
 
+import java.awt.AlphaComposite;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Graphics2D;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.SplashScreen;
 import java.awt.Toolkit;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
@@ -65,8 +69,33 @@ public final class TTY2COMApp extends JFrame {
             }
         });
     }
+    
+    static void renderSplashFrame(Graphics2D g, String info) {
+        g.setComposite(AlphaComposite.Clear);
+        g.fillRect(120,140,200,40);
+        g.setPaintMode();
+        g.setColor(Color.BLACK);
+        g.drawString(info, 120, 150);
+    }
 
     protected void begin() throws Exception {
+        
+        final SplashScreen splash = SplashScreen.getSplashScreen();
+        if (splash == null) {
+            System.out.println("SplashScreen.getSplashScreen() returned null");
+            return;
+        }
+        Graphics2D g = splash.createGraphics();
+        if (g == null) {
+            System.out.println("g is null");
+            return;
+        }
+        
+        renderSplashFrame(g, "Loading serialpundit jar !");
+        splash.update();
+        
+        Thread.sleep(1000);
+        splash.close();
 
         taskExecutor = new TaskExecutor();
         taskExecutor.init();
---
+++ tools-and-utilities/reset_usb_device/bootstrap.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+# This file is part of SerialPundit project and software.
+#
+# Copyright (C) 2014-2016, Rishi Gupta. All rights reserved.
+#
+# The SerialPundit software is DUAL licensed. It is made available under the terms of the GNU Affero 
+# General Public License (AGPL) v3.0 for non-commercial use and under the terms of a commercial 
+# license for commercial use of this software. 
+#
+# The SerialPundit software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+cd "$(dirname "$0")"
+
+command -v libtoolize >/dev/null 2>&1
+if  [ $? -ne 0 ]; then
+    command -v libtool >/dev/null 2>&1
+    if  [ $? -ne 0 ]; then
+        echo "could not find libtool." 1>&2
+        exit 1
+    fi
+fi
+
+command -v autoreconf >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "autoconf and automake are required." 1>&2
+    exit 1
+fi
+
+command -v pkg-config >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "pkg-config is required." 1>&2
+    exit 1
+fi
+
+# The autoreconf runs autoconf, autoheader, aclocal, automake, libtoolize, and autopoint (when appropriate) 
+# repeatedly to update the GNU Build System in the specified directories and their subdirectories.
+autoreconf -vis
+status=$?
+if [ $status -ne 0 ]; then
+    echo "autoreconf exited with status $status" 1>&2
+    exit 1
+fi
+
+echo "Bootstrap script completed successfully."
+exit 0
+
